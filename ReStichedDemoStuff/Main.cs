209e3f8f50ea2903c51dfbd14baee67c9d82ab48
IlovePS3 <149251005+IlovePS3@users.noreply.github.com>
2026-05-17 00:11:45 +0100

    EULA bypass

--- ReStichedDemoStuff/Main.cs
+++ ReStichedDemoStuff/Main.cs
@@ -1,26 +1,51 @@
 using System.Collections;
 using BepInEx;
+using BepInEx.Logging;
+using Bootstrap;
+using HarmonyLib;
 using TMPro;
 using UI;
 using UI.Craftbook.CraftbookGeneration.FunctionButtons;
+using UI.WardrobeDemoSplash;
+using UI.WindowManagement;
 using UnityEngine;
 using UnityEngine.UI;
 using Wardrobe;
+using Object = UnityEngine.Object;
 
 namespace ReStichedDemoStuff;
 
 [BepInPlugin(Constants.Name, Constants.Guid, Constants.Version)]
 public class Main : BaseUnityPlugin
 {
+    public static Main? Instance;
+    public readonly ManualLogSource Logs;
+    
+    private readonly Harmony _harmony;
+    
     private const float GreenscreenAnimTime = .8f;
     public GameObject? wardrobeToolbarRoot;
 
-    private readonly Vector3 _greenscreenTargetPos = new(-1.94f, -205.38f, 73.44f),
-        _greenscreenStartPos = new(-1.94f, -188.9583f, 73.44f);
+    private readonly Vector3 
+        _greenscreenTargetPos = new(-1.94f, -205.38f, 73.44f),
+        _greenscreenStartPos = new(-1.94f, -188.9583f, 73.44f),
+        _standStartPos = Vector3.zero,
+        _standTargetPos = new(0, -10, 0);
 
     private bool _addedCustomButton, _goUp;
-    private GameObject? _greenscreen, _customButtonGo;
+    private GameObject? _greenscreen, _customButtonGo, _stuffyStand;
     private UiManager? _uiManager;
+
+    private Main()
+    {
+        _harmony = new Harmony(Constants.Guid);
+        _harmony.PatchAll();
+        Instance = this;
+        Logs = Logger;
+    }
+
+    private void OnDisable() =>_harmony.UnpatchSelf();
+    
 
 
     private void Update()
@@ -39,6 +64,9 @@ public class Main : BaseUnityPlugin
             else
             {
                 _greenscreen!.SetActive(_greenscreen!.transform.localPosition != _greenscreenStartPos);
+                if (!_stuffyStand)
+                    if (_greenscreen.transform.parent.GetChild(1).GetChild(3).GetChild(0).name == "ThreadBobbin")
+                        _stuffyStand = _greenscreen.transform.parent.GetChild(1).GetChild(3).GetChild(0).gameObject;
             }
         }
         catch
@@ -83,15 +111,36 @@ public class Main : BaseUnityPlugin
 
     private IEnumerator GreenScreenAnim()
     {
-        var from = _goUp ? _greenscreenStartPos : _greenscreenTargetPos;
-        var to = _goUp ? _greenscreenTargetPos : _greenscreenStartPos;
+        var gfrom = _goUp ? _greenscreenStartPos : _greenscreenTargetPos;
+        var gto = _goUp ? _greenscreenTargetPos : _greenscreenStartPos;
+        
+        var pfrom = _goUp ? _standStartPos : _standTargetPos;
+        var pto = _goUp ? _standTargetPos : _standStartPos;
+        
         var elapsed = 0f;
         while (elapsed < GreenscreenAnimTime)
         {
             elapsed += Time.deltaTime;
-            _greenscreen!.transform.localPosition = Vector3.Lerp(from, to, elapsed / GreenscreenAnimTime);
+            _greenscreen!.transform.localPosition = Vector3.Lerp(gfrom, gto, elapsed / GreenscreenAnimTime);
+            _stuffyStand!.transform.localPosition = Vector3.Lerp(pfrom, pto, elapsed / GreenscreenAnimTime);
             yield return null;
         }
-        _greenscreen!.transform.localPosition = to;
+        _greenscreen!.transform.localPosition = gto;
+    }
+    
+    public abstract class Patches
+    {
+        [HarmonyPatch(typeof(WardrobeSplashController))]
+        public abstract class WardrobeSplashPatches
+        {
+            [HarmonyPatch(nameof(WardrobeSplashController.ShowEULAPopup))]
+            [HarmonyPrefix]
+            // ReSharper disable once InconsistentNaming
+            private static bool WindowAwakePatch(WardrobeSplashController __instance)
+            {
+                __instance.buttonsRowAnimator?.gameObject.SetActive(true);
+                return false;
+            }
+        }
     }
 }
