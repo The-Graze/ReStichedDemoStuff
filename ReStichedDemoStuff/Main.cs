d687667554c0b61b93567ac7929c3e7f0b7778a3
IlovePS3 <149251005+IlovePS3@users.noreply.github.com>
2026-05-17 02:59:03 +0100

    Majour changes

--- ReStichedDemoStuff/Main.cs
+++ ReStichedDemoStuff/Main.cs
@@ -1,86 +1,57 @@
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 using BepInEx;
-using BepInEx.Logging;
-using Bootstrap;
 using HarmonyLib;
+using HighLevelWardrobe;
 using TMPro;
+using TrixelCreative.TrixelAudio.Core;
+using TrixelCreative.TrixelAudio.Data;
 using UI;
 using UI.Craftbook.CraftbookGeneration.FunctionButtons;
 using UI.WardrobeDemoSplash;
-using UI.WindowManagement;
 using UnityEngine;
 using UnityEngine.UI;
-using Wardrobe;
-using Object = UnityEngine.Object;
 
 namespace ReStichedDemoStuff;
 
 [BepInPlugin(Constants.Name, Constants.Guid, Constants.Version)]
 public class Main : BaseUnityPlugin
 {
-    public static Main? Instance;
-    public readonly ManualLogSource Logs;
-    
+    private static Main? _instance;
     private readonly Harmony _harmony;
-    
+
     private const float GreenscreenAnimTime = .8f;
+
     public GameObject? wardrobeToolbarRoot;
 
-    private readonly Vector3 
+    private readonly Vector3
         _greenscreenTargetPos = new(-1.94f, -205.38f, 73.44f),
         _greenscreenStartPos = new(-1.94f, -188.9583f, 73.44f),
         _standStartPos = Vector3.zero,
         _standTargetPos = new(0, -10, 0);
 
-    private bool _addedCustomButton, _goUp;
+
+    private SoundEffectAsset? _curtainSound;
+    private TrixelAudioSource? _curtainSoundSource;
+
+    private bool _goUp;
     private GameObject? _greenscreen, _customButtonGo, _stuffyStand;
-    private UiManager? _uiManager;
 
     private Main()
     {
         _harmony = new Harmony(Constants.Guid);
         _harmony.PatchAll();
-        Instance = this;
-        Logs = Logger;
+        _instance = this;
     }
 
-    private void OnDisable() =>_harmony.UnpatchSelf();
-    
-
-
-    private void Update()
+    private void OnDisable()
     {
-        try
-        {
-            if (!_uiManager)
-                _uiManager = FindFirstObjectByType<UiManager>();
-            if (!wardrobeToolbarRoot)
-                wardrobeToolbarRoot = _uiManager.WardrobeState.WardrobeHud!.transform.GetChild(2).gameObject;
-            if (!_greenscreen)
-            {
-                _greenscreen = FindFirstObjectByType<WardrobeOrnamentRenderer>().transform.GetChild(3).gameObject;
-                _greenscreen.transform.localPosition = _greenscreenStartPos;
-            }
-            else
-            {
-                _greenscreen!.SetActive(_greenscreen!.transform.localPosition != _greenscreenStartPos);
-                if (!_stuffyStand)
-                    if (_greenscreen.transform.parent.GetChild(1).GetChild(3).GetChild(0).name == "ThreadBobbin")
-                        _stuffyStand = _greenscreen.transform.parent.GetChild(1).GetChild(3).GetChild(0).gameObject;
-            }
-        }
-        catch
-        {
-            //IGNORE: I only know I can ignore any error's here as I am waiting for the UI and Wardrobe Scene to exist, and I am lazy to hook it up to the UI changes or scene load as each UI has its own callbacks and instance's
-        } 
-
-        if (_addedCustomButton || !_uiManager || !wardrobeToolbarRoot || !_greenscreen) return;
-        AddCustomButton(wardrobeToolbarRoot!);
+        _harmony.UnpatchSelf();
     }
 
-    private void AddCustomButton(GameObject toolbarRoot)
+    private void AddCustomButton()
     {
-        var buttonsContainer = toolbarRoot.transform.GetChild(0).GetChild(4).GetChild(0);
+        var buttonsContainer = wardrobeToolbarRoot?.transform.GetChild(0).GetChild(4).GetChild(0);
 
         if (!buttonsContainer) return;
 
@@ -98,48 +69,92 @@ public class Main : BaseUnityPlugin
 
         var customButton = _customButtonGo.GetComponentInChildren<Button>();
         customButton.onClick.AddListener(ToggleGreenScreen);
-
-        _addedCustomButton = true;
     }
 
     private void ToggleGreenScreen()
     {
         _goUp = !_goUp;
+        _curtainSoundSource?.PlayOneShot(_curtainSound!, false);
+        _greenscreen!.SetActive(true);
         StartCoroutine(GreenScreenAnim());
     }
 
 
     private IEnumerator GreenScreenAnim()
     {
-        var gfrom = _goUp ? _greenscreenStartPos : _greenscreenTargetPos;
-        var gto = _goUp ? _greenscreenTargetPos : _greenscreenStartPos;
-        
-        var pfrom = _goUp ? _standStartPos : _standTargetPos;
-        var pto = _goUp ? _standTargetPos : _standStartPos;
-        
+        var greenscreenFrom = _goUp ? _greenscreenStartPos : _greenscreenTargetPos;
+        var greenscreenTo = _goUp ? _greenscreenTargetPos : _greenscreenStartPos;
+
+        var standFrom = _goUp ? _standStartPos : _standTargetPos;
+        var standTo = _goUp ? _standTargetPos : _standStartPos;
+
         var elapsed = 0f;
         while (elapsed < GreenscreenAnimTime)
         {
             elapsed += Time.deltaTime;
-            _greenscreen!.transform.localPosition = Vector3.Lerp(gfrom, gto, elapsed / GreenscreenAnimTime);
-            _stuffyStand!.transform.localPosition = Vector3.Lerp(pfrom, pto, elapsed / GreenscreenAnimTime);
+            _greenscreen!.transform.localPosition = Vector3.Lerp(greenscreenFrom, greenscreenTo, elapsed / GreenscreenAnimTime);
+            _stuffyStand!.transform.localPosition = Vector3.Lerp(standFrom, standTo, elapsed / GreenscreenAnimTime);
             yield return null;
         }
-        _greenscreen!.transform.localPosition = gto;
+        _greenscreen!.transform.localPosition = greenscreenTo;
+        _greenscreen!.SetActive(_greenscreen!.transform.localPosition != _greenscreenStartPos);
     }
-    
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public abstract class Patches
     {
         [HarmonyPatch(typeof(WardrobeSplashController))]
         public abstract class WardrobeSplashPatches
         {
+            [HarmonyPatch(nameof(WardrobeSplashController.Awake))]
+            [HarmonyPostfix]
+            private static void WardrobeSplashAwakePatch(WardrobeSplashController __instance)
+            {
+                if(!_instance) return;
+                
+                if(!_instance._curtainSound)
+                    _instance._curtainSound = __instance.curtainOpenSound;
+                
+                if(_instance._curtainSoundSource) return;
+                _instance._curtainSoundSource = Instantiate(__instance.audioSource);
+                _instance._curtainSoundSource.transform.position = __instance.audioSource.transform.position;
+                _instance._curtainSoundSource.transform.rotation = __instance.audioSource.transform.rotation;
+            }
+            
             [HarmonyPatch(nameof(WardrobeSplashController.ShowEULAPopup))]
             [HarmonyPrefix]
-            // ReSharper disable once InconsistentNaming
-            private static bool WindowAwakePatch(WardrobeSplashController __instance)
+            private static bool EULAPatch(WardrobeSplashController __instance)
             {
-                __instance.buttonsRowAnimator?.gameObject.SetActive(true);
+                //Skips the check and animates the Splash buttons in
+                __instance.StartCoroutine(__instance.AnimateInButtons());
                 return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(WardrobeManager))]
+        public abstract class WardrobeManagerPatches
+        {
+            [HarmonyPatch(nameof(WardrobeManager.SetupWardrobe))]
+            [HarmonyPostfix]
+            private static void WardrobeSetupPatch(WardrobeManager __instance)
+            {
+                if(!_instance) return;
+                _instance._greenscreen = __instance.transform.GetChild(3).gameObject;
+                _instance._greenscreen.transform.localPosition = _instance._greenscreenStartPos;
+                _instance._stuffyStand = _instance._greenscreen.transform.parent.GetChild(1).GetChild(3).GetChild(0).gameObject;
+            }
+        }
+
+        [HarmonyPatch(typeof(UiManager))]
+        public abstract class UiManagerPatches
+        {
+            [HarmonyPatch(nameof(UiManager.InstantiateWardrobeUi))]
+            [HarmonyPostfix]
+            private static void InstantiateWardrobeUiPatch(UiManager __instance)
+            {
+                if(!_instance) return;
+                _instance.wardrobeToolbarRoot = __instance.WardrobeState.WardrobeHud?.transform.GetChild(2).gameObject;
+                _instance.AddCustomButton();
             }
         }
     }
